889318af20dfe219d46037a08d4fb32e76366f2c
casperr29 <arelylarag329@gmail.com>
2023-11-12 17:14:34 -0600

    si

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const express = require('express');
-const db = require('./db');
-const routerApi = require('./app/routes');
-const cors = require('cors');
+const express = require("express");
+const routerApi = require("./app/routes");
+const cors = require("cors");
 const {
   errorHandler,
   logErrors,
   boomErrorHandler,
-} = require('./app/middlewares/error.handler');
-const { DBURL } = require('./const.json');
+} = require("./app/middlewares/error.handler");
+
+const db = require("./db");
+const { DBURL } = require("./const.json");
 //HACEMOS LA CONEXIÓN
 db(DBURL);
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+const whitelist = [
+  "http://localhost:8080",
+  "https://myapp.co",
+  "http://localhost:3000",
+  "https://programacionwebiiback-end-production.up.railway.app",
+];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  },
+};
+
+app.use(cors(options));
 app.use(
   express.json(
     { extended: false } // permite codificar matrices y objetos enriquecidos en formato codificado en url
   )
 ); //Selección de tipo de analisis de datos
-app.use(express.static('app/storage')); //Sacarlos Recursos estaticos de esta carpeta
+app.use(express.static("app/storage")); //Sacarlos Recursos estaticos de esta carpeta
 
 //app.use(customHeader);
 routerApi(app);
@@ -30,5 +47,5 @@ app.use(errorHandler); //Los middlewares de error van despues del routing
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Mi PORT ' + PORT);
+  console.log("Mi PORT " + PORT);
 });
